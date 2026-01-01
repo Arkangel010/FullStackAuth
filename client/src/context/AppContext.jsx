62118a6c6dd736d09f4ddc23836cd1b010b435de
Kumar Aman <117035705+Arkangel010@users.noreply.github.com>
2026-01-01 10:18:42 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -23,7 +23,7 @@ export const AppContextProvider = (props) =>{
     }
     const getUserData = async () =>{
         try{
-            const {data} = await axios.get(backendUrl + '/api/user/data')
+            const {data} = await axios.get(backendUrl + '/api/user/data', {withCredentials: true})
             console.log(data); 
             data.success ? setUserData(data.userData) : toast.error(data.message)
         }
